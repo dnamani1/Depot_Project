2cb8b48510c5ecb78ae9c75ee3f2ee0cf5ddc942
Deeksha Namani <dnamani1@my.westga.edu>
2024-02-04 10:43:33 -0500

    Chapter 8: Worked on catalog Display.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+  get 'store/index', to: 'store#index'
   resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in
+  # https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
