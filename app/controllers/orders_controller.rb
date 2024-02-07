c8e31f5a747edc2451a34a461ed619a4cd52a6c3
Deeksha Namani <dnamani1@my.westga.edu>
2024-02-07 09:00:08 -0500

    Set up our Rails app's environments for outbound email, designed a mailer for order confirmations in text and HTML, implemented Active Job for background processing to improve user experience, and expanded our system test to ensure the workflow, including email dispatch, functions as intended.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,17 +29,17 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to store_index_url, notice:
+        ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
+        format.html { redirect_to store_index_url, notice: 
           'Thank you for your order.' }
         format.json { render :show, status: :created,
           location: @order }
-
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json { render json: @order.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -84,4 +84,16 @@ class OrdersController < ApplicationController
          redirect_to store_index_url, notice: 'Your cart is empty'
        end
      end
+
+     def pay_type_params
+      if order_params[:pay_type] == "Credit card"
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == "Check"
+        params.require(:order).permit(:routing_number, :account_number)
+      elsif order_params[:pay_type] == "Purchase order"
+        params.require(:order).permit(:po_number)
+      else
+        {}
+      end
+    end
 end
